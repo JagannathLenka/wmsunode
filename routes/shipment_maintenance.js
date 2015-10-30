2a7317f40315e565e9eecd169c1f2e590b760ee5
Jagannath Lenka <JagannathLenka@Jagannaths-MacBook-Pro.local>
2015-10-29 19:50:02 -0700

    fix the shipment

--- routes/shipment_maintenance.js
+++ routes/shipment_maintenance.js
@@ -4,22 +4,15 @@ var router = express.Router();
 var http = require('http');
 
 /* GET home page. */
-router.get('/:id', function(req, response, next) {
+router.get('/', function(req, response, next) {
   
   var optionsget = {
     host : 'wmsservice.herokuapp.com', // here only the domain name
-    port : 3001,
+    //port : 3001,
+    path :  '/shipment/?client=WM&warehouse=WH1',
     method : 'GET' // do GET
   };
  // do the GET request
-
-    if (req.params.id != null) {
-      optionsget.path = '/shipment/' + req.params.id + '?client=WM&warehouse=WH1'
-    }
-    else {
-      optionsget.path =  '/shipment/?client=WM&warehouse=WH1' 
-    }
-
 
   var reqGet = http.request(optionsget, function(res) {
     console.log("statusCode: ", res.statusCode);
@@ -30,7 +23,7 @@ router.get('/:id', function(req, response, next) {
     });
  
   res.on('end', function() {
-      console.log(responseString);
+      //console.log(responseString);
       var responseObject = JSON.parse(responseString);
       response.json(responseObject);
     });
@@ -44,19 +37,20 @@ router.get('/:id', function(req, response, next) {
 
 });
 
-
-/* GET home page. */
-router.get('/', function(req, response, next) {
+router.get('/:id', function(req, response, next) {
   
+  console.log(req.params.id)
+
   var optionsget = {
-    host : 'localhost', // here only the domain name
-    port : 3001,
-    path : '/shipment/?client=WM&warehouse=WH1', // the rest of the url with parameters if needed
+    host : 'wmsservice.herokuapp.com', // here only the domain name
+    //port : 3001,
+    path :  '/shipment/'+ req.params.id + '?client=WM&warehouse=WH1',
     method : 'GET' // do GET
   };
  // do the GET request
+
   var reqGet = http.request(optionsget, function(res) {
-    console.log("statusCode: ", res.statusCode);
+    //console.log("statusCode: ", res.statusCode);
  
   var responseString = '';
     res.on('data', function(d) {
